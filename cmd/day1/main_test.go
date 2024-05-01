a4c8988b49e4c2f544c3be6db6805897b5f797f0
Hannes Hultergård <hannes.hultergard@gmail.com>
2024-05-01 11:23:32 +0200

    Add test for day 1 part 2

--- cmd/day1/main_test.go
+++ cmd/day1/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-var testInput = `1abc2
+var testInput1 = `1abc2
 fdas2p3u5i2a
 22fdas
 f8dda3a2
@@ -12,12 +12,31 @@ qw0x
 rerreqa
 fdd3af3a`
 
-var expected = 183
+var expected1 = 183
+
+var testInput2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+eighthree`
+
+var expected2 = 364
 
 func TestPart1(t *testing.T) {
-  result := part1(testInput)
+	result := part1(testInput1)
 
-  if result != expected {
-    t.Errorf("part1() = %d; want %d", result, expected)
-  }
+	if result != expected1 {
+		t.Errorf("part1() = %d; want %d", result, expected1)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := part2(testInput2)
+
+	if result != expected2 {
+		t.Errorf("part2() = %d; want %d", result, expected2)
+	}
 }
